4296f428a6a105b58250775dddb86ed9ed373d05
Alex Woods <alextwoods@outlook.com>
2020-03-12 15:03:14 -0700

    Print out git location

--- entrypoint.rb
+++ entrypoint.rb
@@ -4,4 +4,6 @@ puts "Hello #{ARGV[0]}"
 puts ENV.inspect
 puts "-----------------------\n\n"
 puts `ls /github/workspace`
+puts "*************************``"
+puts "GIT: #{`which git`}"
 puts "::set-output name=time::#{Time.now}"
