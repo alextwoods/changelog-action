8dba6504e5e2a11c606b70ff4fffc30dd2ae24f5
Alex Woods <alextwoods@outlook.com>
2020-03-19 09:58:22 -0700

    Update to set outputs

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # changelog-action
+
+## Testing locally
+Build the image `docker image build ./ -t changelog_action`
+
+You need the sdk checked out locally (Replace FUL_PATH_TO_SDK)
+`docker run --name changelog_action --workdir /github/workspace -v "FUL_PATH_TO_SDK/aws-sdk-ruby":"/github/workspace" changelog_action`
--- entrypoint.rb
+++ entrypoint.rb
@@ -1,21 +1,38 @@
 #!/usr/local/bin/ruby
 
 require 'rake'
-
-puts "Hello #{ARGV[0]}"
-puts ENV.inspect
-puts "-----------------------\n\n"
-puts `ls /github/workspace`
-puts "*************************``"
-puts "GIT: #{`which git`}"
-
+require 'json'
 
 Dir.chdir '/github/workspace'
+
+# TODO: Use information from the github workflow to determine what the base should be
+review_base = 'master'
+changes = `git diff --name-only #{review_base}`.split("\n")
+
+puts "Changes: #{changes}"
+
+build_tool_changes = changes.any? { |f| f.start_with? 'build_tools' }
+
+gems_changed = changes.map do |f|
+  m = f.match(/gems\/(aws-sdk-[a-z0-9]+)\//)
+  if m && m.captures
+    m.captures[0]
+  end
+end.compact
+
+core_changed = gems_changed.include? 'aws-sdk-core'
+
+
 puts "Attempting to Build"
 puts load('Rakefile')
 puts "Loading Rakefile.  Tryign to invoke...."
 Rake::Task['build'].invoke
-changes = `git status --porcelain`
+generated_changes = `git status --porcelain`
+
 puts "\n\n-------------------------"
-puts changes
-puts "::set-output name=changes::#{changes}"
+puts generated_changes
+puts "::set-output name=files_changed::#{JSON.dump(changes)}"
+puts "::set-output name=build_tool_changes::#{JSON.dump(build_tool_changes)}"
+puts "::set-output name=gems_changed::#{JSON.dump(gems_changed)}"
+puts "::set-output name=core_changed::#{JSON.dump(core_changed)}"
+puts "::set-output name=generated_changes::#{JSON.dump(generated_changes)}"
